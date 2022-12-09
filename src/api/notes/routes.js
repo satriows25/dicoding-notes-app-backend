5d026e197f09b7ba43dc1aaaae0ccdf2fc9752de
Satrio Wicaksono <88221074+satriows25@users.noreply.github.com>
2022-12-09 16:49:28 +0700

    Implementasi Autentikasi (#5)

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -2,27 +2,42 @@ const routes = (handler) => [
   {
     method: 'POST',
     path: '/notes',
-    handler: (request, h) => handler.postNoteHandler(request, h)
+    handler: (request, h) => handler.postNoteHandler(request, h),
+    options: {
+      auth: 'notesapp_jwt'
+    }
   },
   {
     method: 'GET',
     path: '/notes',
-    handler: () => handler.getNotesHandler()
+    handler: () => handler.getNotesHandler(),
+    options: {
+      auth: 'notesapp_jwt'
+    }
   },
   {
     method: 'GET',
     path: '/notes/{id}',
-    handler: (request) => handler.getNoteByIdHandler(request)
+    handler: (request) => handler.getNoteByIdHandler(request),
+    options: {
+      auth: 'notesapp_jwt'
+    }
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
-    handler: (request) => handler.putNoteByIdHandler(request)
+    handler: (request) => handler.putNoteByIdHandler(request),
+    options: {
+      auth: 'notesapp_jwt'
+    }
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
-    handler: (request) => handler.deleteNoteByIdHandler(request)
+    handler: (request) => handler.deleteNoteByIdHandler(request),
+    options: {
+      auth: 'notesapp_jwt'
+    }
   }
 ];
 
